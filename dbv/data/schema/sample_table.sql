ef0baad62ac084f2f04f312f376260a6a90ce077
johnotaalo <john.otaalo@strathmore.edu>
2014-07-25 00:24:14 +0300

    Database Versioning Tool added

---
+++ dbv/data/schema/sample_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `sample_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `details` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
