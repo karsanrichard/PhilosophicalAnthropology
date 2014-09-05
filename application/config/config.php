c3a474abbd5fb45fa963aa100c66491fac1fb5d9
johnotaalo <john.otaalo@strathmore.edu>
2014-09-05 15:33:36 +0300

    changes to videos

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Philosophical_Anthropology/';
+$config['base_url']	= 'http://localhost/philosophy/';
 
 /*
 |--------------------------------------------------------------------------
