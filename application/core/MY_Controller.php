26ef9bf6aec23b686869a5b3fd68fc47dba620ab
bakasajoshua <baksajoshua09@gmail.com>
2014-09-05 16:03:06 +0300

    freedom

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Controller extends MX_Controller
+{
+	function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+
+	function index()
+	{
+		
+	}
+}
---
+++ application/modules/content/views/sexuality_view.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	<title>Human Sexuality</title>
+	</head>
+	<body>
+		<div>
+			<div>
+				<h3>Human Love and Sexuality</h3>
+				<div>
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
