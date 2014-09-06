7bee809e5cba79c390f8dace83660a154408ff7e
bakasajoshua <baksajoshua09@gmail.com>
2014-09-06 14:35:30 +0300

    human sexuality

--- application/modules/content/views/sexuality_view.php
+++ application/modules/content/views/sexuality_view.php
@@ -5,9 +5,92 @@
 	<body>
 		<div>
 			<div>
-				<h3>Human Love and Sexuality</h3>
+				<h2>Human Love and Sexuality</h2>
 				<div>
 					<p></p>
+				</div>
+				<div>
+					<h3>The two modalities of the human person</h3>
+					<p>As human beings we need to discover who we are:</p>
+					<ul>
+						<li>To know our personal identity.</li>
+						<li>What is our core; what really lies within our hearts.</li>
+					</ul>
+					<p>Facts of life is that as humans we received the gift of life as we are born. In this life there is the physical level(animal level) and personal level.</p>
+					<p>At the physical level there is:</p>
+					<ul>
+						<li>Male</li>
+						<li>Female</li>
+					</ul>
+					<p>Male and female refers to the sex which refers to the biological differences; chromosomes, hormonal profiles, internal and external sex organs.</p>
+					<p>At the personal level there is the:-</p>
+					<ul>
+						<li>Masculine</li>
+						<li>Feminine</li>
+					</ul>
+					<p>Masculine and feminine refers to the gender which describes the characteristics that a society or culture delineates as masculine or feminine.</p>
+				</div>
+				<div>
+					<h3>Elements of our masculine/feminine identity</h3>
+					<p><strong>Relational: </strong></p>
+					<p><strong>Diversity: </strong> is a concept that encompases acceptance and respect. It means understanding that each individual is unique. It is about understanding each other and moving beyond simple tolerance to embracing and celebrating the rich dimensions of diversity contained within each individual<a href="http://gladstone.uoregon.edu/~asuomca/diversityinit/definition.html" target="blank">(gladstone, 2014)</a>.</p>
+					<p><strong>Creativity: </strong></p>
+					<p><strong>Complementarity: </strong> We need each other to be fully humna as men and women</p>
+				</div>
+				<div>
+					<h3>Power of attraction</h3>
+					<h5>Nature of masculine/ feminine attraction</h5>
+					<p>The female and mala attraction to each other is absolutely physical since this kind of attraction is at the animal level</p>
+					<p>Physical attraction in men focuses only on the physical characteristics of women. It is blind to the character and spiritual qualities that make up feminity and it is a sign of humanly underdeveloped man!</p>
+					<p>Physical attraction in women only wnats to draw men's attention to her female body. There is absolutely no interest in(does not understand) the nature, challenge and power of the feminine soul. This is a sign of a humanly underdeveloped woman!</p>
+					<h5>Understanding the power of attraction</h5>
+					<p>The power of the <u>female</u> generates physical desire in the male: once satisfied he withdraws into that satisfaction, into himself, until the desire is aroused again. The power of the <u>feminine</u> generates respect that can grow, even to a form of veneration, which draws a man out of himself and inspires him to higher things<a href="www.cormacburke.or.ke" target="blank">(C. Burke)</a></p>
+					<h6>Some of the Masculine and feminine Identity are as below</h6>
+					<div>
+						<h5>Masculine Identity</h5>
+						<ul>
+							<li>Courageous</li>
+							<li>Able to cherish/ be gentle with a lady</li>
+							<li>Protective</li>
+							<li>Responsible (head of family; pay fees)/ decisive/ leadership</li>
+							<li>Logical/ Rational</li>
+							<li>Open to challenge/ risk-takers</li>
+							<li>Confident</li>
+							<li>Diligent</li>
+							<li>Discerning</li>
+							<li>Reasoning</li>
+							<li>Distinction</li>
+							<li>Boundaries</li>
+							<li>Discriminating</li>
+							<li>Structure</li>
+						</ul>
+					</div>
+					<div>
+						<h5>Feminine Identity</h5>
+						<ul>
+							<li>Caring, Loving, soft-spoken</li>
+							<li>Hospitable, home-makers, care/nurture other people</li>
+							<li>Multi-tasking</li>
+							<li>Emotional</li>
+							<li>`Contain`/ make the most of what a spouse can bring home</li>
+							<li>Supportive/ submissive</li>
+							<li>Pretenders</li>
+							<li>More dependant</li>
+							<li>Intuituve</li>
+							<li>Unifying</li>
+							<li>Wholeness</li>
+							<li>Beauty</li>
+							<li>Process</li>
+						</ul>
+					</div>
+					<p>For a person`s personal growth, one needs to be aware of their own sexual identity, male or female. Not only to know it but also to accept it. After acceptance then integrate it into their own personality. He/she should then be aware of the sexual identity of the other half of the human race(male/female). They should know it then accept and appreciate it and learn from it. This is because neither man nor woman alone offers a full, perfect model of humanity. Each is a partial model of human being, we need the other half to give the full picture about what it means to be truly human, to create a truly human society!</p>
+					<p>The <strong>Challenge</strong> comes:-
+						<ul> 
+							<li>in knowing and discover each other in our originalty and respect our legitimate differnces</li>
+							<li>Accepting and appreciate them; maintaina sense of mutual help and the complementary roles of men and women</li>
+							<li>Difference of gender gives rise to particular ways of relating to each other ina variety of situations. Learn how to draw the best from each other in each type of relationship!</li>
+						</ul>
+					</p>
 				</div>
 			</div>
 		</div>
