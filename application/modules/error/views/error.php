8775e45ce23c452166e073e8aa88e1db1ccaba61
johnotaalo <john.otaalo@strathmore.edu>
2014-09-06 11:08:04 +0300

    Added Flat UI, Semantic and the error 404 page

---
+++ application/modules/error/views/error.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/bootstrap/css/bootstrap.css">
+<!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/bootstrap/css/bootstrap.min.css"> -->
+<link rel="stylesheet" href="<?php echo base_url();?>assets/semantic/packaged/css/semantic.css">
+<div class="jumbotron" id = "home-div">
+	<div class = "row">
+		<div id = "jumbo-instruction" class = "col-md-7 col-md-offset-4" style="text-align: centered;">
+			<h3>Error 404</h3>
+			<p>
+				This page could not be Found
+			</p>
+			<p><a class="btn btn-primary btn-lg" role="button">Back to Home Page</a></p>
+		</div>
+	</div>
+</div>
